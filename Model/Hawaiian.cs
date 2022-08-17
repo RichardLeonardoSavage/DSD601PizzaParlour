ca1daf76656178ae273c84a7f3ba1b97eede4151
RichardLeonardoSavage <savagerichard100@gmail.com>
2022-08-18 10:25:14 +1200

    Add project files.

---
+++ Model/Hawaiian.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DSD601PizzaParlour.Model
+{
+    public class Hawaiian : Pizza
+    {
+        [Display (Name = "Hawaiian")]
+        public string PizzaName { get; set; }
+    }
+}
---
+++ Model/Pizza.cs
@@ -0,0 +1,27 @@
+﻿namespace DSD601PizzaParlour.Model
+{
+    public class Pizza
+    {
+       
+        public string Image { get; set; }
+        public string PizzaChef { get; set; }
+
+        public string Description { get; set; }
+
+        public List<string> ListOfIngredients { get; set; }
+
+        public List<string> ListOfSizesAndPrices { get; set; }
+
+        public string PizzaName { get; set; }
+        
+        public bool isGlutenFree { get; set; }
+
+        public bool isVegeterian { get; set; }
+
+        public Pizza()
+        {
+            ListOfIngredients = new List<string>();
+            ListOfSizesAndPrices = new List<string>();
+        }
+    }
+}
